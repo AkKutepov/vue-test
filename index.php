cd706f3888ad34ded405aa73e7e52237843e0116
AkKutepov <info@repnrep.com>
2023-06-30 20:02:16 +0300

    cosmetic

--- index.php
+++ index.php
@@ -5,13 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Vue Example</title>
 
-    <!--+->
-    <link rel="stylesheet" href="./css/bootstrap.css"> <!-- 4.6.2 -->
     <!---->
+    <link rel="stylesheet" href="./css/bootstrap.css">
     <link rel="stylesheet" href="./css/main.css">
+    <!---->
+
     <!---->
     <script src="./ext/vue.global.min.js"></script> <!-- 3.2.47 -->
     <script src="./ext/vue-router.global.min.js"></script> <!-- 4.1.6 -->
+    <!---->
+    
     <!--+->
     <script src="./ext/axios.min.js"></script>  <!-- 1.3.3 -->
     <!---->
@@ -20,12 +23,12 @@
   <body>
     <div id="app"><Navbar /></div>
     
-    <!---->
+    <!--+->
     <script src="./dist/com/weather.js"></script>
     <script src="./dist/bundle.js"></script>
     <!---->
 
-    <!--+->
+    <!---->
     <script type='module'>
       import './src/weather.js'
       import "./src/main.js"
